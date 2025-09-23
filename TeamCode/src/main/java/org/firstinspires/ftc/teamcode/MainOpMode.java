9d64db280b00a8d3176c3c5e0e9a068ff63ccd5f
Suman <sumanmuppavarapu@gmail.com>
2025-09-23 13:27:42 -0500

    apriltag turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -28,12 +28,30 @@ package org.firstinspires.ftc.teamcode;/* Copyright (c) 2017 FIRST. All rights r
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import android.util.Size;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 /*
  * This file contains an example of an iterative (Non-Linear) "OpMode".
@@ -49,8 +67,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Disabled
-@TeleOp(name="Main OpMode", group="Iterative OpMode")
+@TeleOp(name="Main Main OpMode", group="Iterative OpMode")
 public class MainOpMode extends OpMode
 {
     // Declare OpMode members.
@@ -59,13 +76,48 @@ public class MainOpMode extends OpMode
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor rightBack = null;
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+
+
+    private Follower follower;
+    private boolean following = false;
+    private final Pose TARGET_LOCATION = new Pose();
+
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+    private boolean targetFound = false;
+    private AprilTagDetection desiredTag = null;
+    private static final int DESIRED_TAG_ID = 20;
+
+    boolean lb1Pressed = false;
+    boolean rb1Pressed = false;
+    boolean b1Pressed = false;
+    boolean a1Pressed = false;
+    boolean x1Pressed = false;
+    boolean y1Pressed = false;
+    boolean down1Pressed = false;
+    boolean up1Pressed = false;
+    boolean right1Pressed = false;
+    boolean left1Pressed = false;
+
+    boolean lb2Pressed = false;
+    boolean rb2Pressed = false;
+    boolean b2Pressed = false;
+    boolean a2Pressed = false;
+    boolean x2Pressed = false;
+    boolean y2Pressed = false;
+    boolean down2Pressed = false;
+    boolean up2Pressed = false;
+    boolean right2Pressed = false;
+    boolean left2Pressed = false;
+
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
+        initAprilTag();
 
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(follower.getPose());
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
@@ -106,47 +158,193 @@ public class MainOpMode extends OpMode
      */
     @Override
     public void loop() {
+        follower.update();
+        targetFound = false;
+        desiredTag  = null;
+
+        // Step through the list of detected tags and look for a matching tag
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                    // Yes, we want to use this tag.
+                    targetFound = true;
+                    desiredTag = detection;
+                    break;  // don't look any further.
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
+
+        //- DRIVE STUFF
         double max;
 
         // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-        double lateral =  gamepad1.left_stick_x;
-        double yaw     =  gamepad1.right_stick_x;
+        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
 
         // Combine the joystick requests for each axis-motion to determine each wheel's power.
         // Set up a variable for each drive wheel to save the power level for telemetry.
-        double leftFrontPower  = axial + lateral + yaw;
+        double leftFrontPower = axial + lateral + yaw;
         double rightFrontPower = axial - lateral - yaw;
-        double leftBackPower   = axial - lateral + yaw;
-        double rightBackPower  = axial + lateral - yaw;
+        double leftBackPower = axial - lateral + yaw;
+        double rightBackPower = axial + lateral - yaw;
 
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
+        if(gamepad1.b && !b1Pressed && targetFound) {
+            double tagBearing = desiredTag.ftcPose.bearing;
 
-        if (max > 1.0) {
-            leftFrontPower  /= max;
-            rightFrontPower /= max;
-            leftBackPower   /= max;
-            rightBackPower  /= max;
+            // Current robot heading
+            double currentHeading = follower.getPose().getHeading();
+
+            // Target heading is current + bearing
+            double targetHeading = currentHeading + tagBearing;
+
+            if (!following) {
+                // Tell Pedro follower to rotate to that heading
+                follower.turnTo(Math.toRadians(targetHeading));
+                following = true;
+            }
+            telemetry.addData("AHH", "HAPPENING");
         }
-        // Send calculated power to wheels
-        leftFront.setPower(leftFrontPower);
-        rightFront.setPower(rightFrontPower);
-        leftBack.setPower(leftBackPower);
-        rightBack.setPower(rightBackPower);
+        else if (!gamepad1.b) {
+
+            telemetry.addData("b sucks", "BOOOOOO");
+        }
+        else if (b1Pressed) {
+
+            telemetry.addData("it all sucks", "BOOOOOO");
+        }
+        else if (!targetFound) {
+
+            telemetry.addData("AHH", "BOOOOOO");
+        }
+        else if (!following){
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+            // Send calculated power to wheels
+            leftFront.setPower(leftFrontPower);
+            rightFront.setPower(rightFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightBack.setPower(rightBackPower);
+
+            telemetry.addData("bleh", "BOOOOOO");
+        }
+
+        if (following && !follower.isBusy()) following = false;
+
+        //update all button states
+        b1Pressed = gamepad1.b;
+        a1Pressed = gamepad1.a;
+        x1Pressed = gamepad1.x;
+        y1Pressed = gamepad1.y;
+        down1Pressed = gamepad1.dpad_down;
+        up1Pressed = gamepad1.dpad_up;
+        left1Pressed = gamepad1.dpad_left;
+        right1Pressed = gamepad1.dpad_right;
+        lb1Pressed = gamepad1.left_bumper;
+        rb1Pressed = gamepad1.right_bumper;
+
+        b2Pressed = gamepad2.b;
+        a2Pressed = gamepad2.a;
+        x2Pressed = gamepad2.x;
+        y2Pressed = gamepad2.y;
+        down2Pressed = gamepad2.dpad_down;
+        up2Pressed = gamepad2.dpad_up;
+        left2Pressed = gamepad2.dpad_left;
+        right2Pressed = gamepad2.dpad_right;
+        lb2Pressed = gamepad2.left_bumper;
+        rb2Pressed = gamepad2.right_bumper;
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-    }
 
+
+    }
     /*
      * Code to run ONCE after the driver hits STOP
      */
+
+    private void initAprilTag() {
+
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+
+                // The following default settings are available to un-comment and edit as needed.
+                //.setDrawAxes(false)
+                //.setDrawCubeProjection(false)
+                .setDrawTagOutline(true)
+                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+                // ... these parameters are fx, fy, cx, cy.
+
+                .build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(3);
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }
     @Override
     public void stop() {
     }
